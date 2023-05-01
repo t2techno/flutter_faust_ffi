ddcebd9de7b692310f54c9620d6d38fdb6749dd3
t2techno <ttuschhoff11@gmail.com>
2023-04-30 22:41:21 -0500

    generated dll file and typedefs for dsp_api

---
+++ lib/dsp_dart/api_types.dart
@@ -0,0 +1,103 @@
+import 'dart:ffi';
+
+/* DSP Stuff
+
+typedef struct {
+	int iVec0[2];
+	int fSampleRate;
+	float fConst1;
+	float fRec0[2];
+	float fVec1[2];
+	int IOTA0;
+	float fVec2[512];
+	int iConst4;
+	int iConst5;
+	float fConst7;
+	float fConst8;
+	float fConst9;
+	float fConst10;
+	float fRec1[2];
+	float fConst11;
+} mydsp;
+*/
+class MyDsp extends Struct {
+  external Pointer<int> iVec0; //int iVec0[2];
+
+  @Int32()
+  external int fSampleRate; //int fSampleRate;
+
+  @Float()
+  external double fConst1; //float fConst1;
+
+  external Pointer<double> fRec0; //float fRec0[2];
+
+  external Pointer<double> fVec1; //float fVec1[2];
+
+  @Int32()
+  external int IOTA0; //int IOTA0;
+  
+  external Pointer<double>fVec2; //float fVec2[512];
+  
+  @Int32()
+  external int iConst4;// int iConst4;
+
+  @Int32()
+  external int iConst5;	// int iConst5;
+
+  @Float()
+  external double fConst7; // float fConst7;
+
+  @Float()
+  external double fConst8;  // float fConst8;
+
+  @Float()
+  external double fConst9; // float fConst9;
+
+  @Float()
+  external double fConst10; // float fConst10;
+
+  external Pointer<double> fRec1fRec1; // float fRec1[2];
+
+  @Float()
+  external double fConst11;// float fConst11;
+}
+
+// mydsp* newmydsp()
+typedef NewMyDsp = Pointer<MyDsp> Function();
+
+// 	void initmydsp(mydsp* dsp, int sample_rate)
+typedef InitMyDspNative = Pointer<void> Function(Pointer<MyDsp> dsp, Int32 sample_rate);
+typedef InitMyDsp = Pointer<void> Function(Pointer<MyDsp> dsp, int sample_rate);
+
+// void buildUserInterfacemydsp(mydsp* dsp, UIGlue* ui_interface)
+typedef BuildUserInterfaceMyDsp = Pointer<void> Function(Pointer<MyDsp> dsp, Pointer<UiGlue> ui_interface);
+
+// 	void computemydsp(mydsp* dsp, int count, FAUSTFLOAT** RESTRICT inputs, FAUSTFLOAT** RESTRICT outputs);
+typedef AudioIOArrayNative = Pointer<Pointer<Float>>;
+typedef AudioIOArray = Pointer<Pointer<double>>;
+typedef ComputeMyDspNative = Pointer<void> Function(Pointer<MyDsp> dsp, Int32 count, AudioIOArrayNative inputs, AudioIOArrayNative outputs);
+typedef ComputeMyDsp = Pointer<void> Function(Pointer<MyDsp> dsp, int count, AudioIOArray inputs, AudioIOArray outputs);
+
+// 	void deletemydsp(mydsp* dsp)
+typedef DeleteMyDsp = Pointer<void> Function(Pointer<MyDsp> dsp);
+
+
+/* UI and Meta data stuff */
+
+//typedef void (* openVerticalBoxFun) (void* ui_interface, const char* label);
+typedef OpenVerticalBoxFun = Pointer<void> Function(Pointer<void> ui_interface, const Pointer<Utf8> label);
+
+//typedef void (* closeBoxFun) (void* ui_interface);
+typedef CloseBoxFun = Pointer<void> Function(Pointer<void> ui_interface);
+
+class UiGlue extends Struct {
+    external OpenVerticalBoxFun openVerticalBoxFun;
+    external CloseBoxFun closeBoxFun;
+}
+
+//typedef void (* metaDeclareFun) (void* ui_interface, const char* key, const char* value);
+typedef MetaDeclareFun = Pointer<void> Function(Pointer<void> ui_interface, const Pointer<Utf8> key, const Pointer<Utf8> value);
+class MetaGlue extends Struct {
+    external Pointer<void> metaInterface;
+    external MetaDeclareFun declare;
+}
